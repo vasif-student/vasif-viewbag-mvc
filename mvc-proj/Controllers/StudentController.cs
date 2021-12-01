db205db3516382863c48d471ab16befc2310d357
vasif-student <vasifah@code.edu.az>
2021-12-01 17:31:33 +0400

    mvc-homework

---
+++ mvc-proj/Controllers/StudentController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvc_proj.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index(int? id)
+        {
+            if(id == null)
+            {
+                ViewBag.Message = "ID is null";
+                return View();
+            }
+            return Json(new { name = "Emin", surname = "Eliyev", Age = 25 });
+        }
+    }
+}
